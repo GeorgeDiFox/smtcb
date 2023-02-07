52596e0021c022d436a5b4db2964ff8291afac42
GeorgeDiFox <124592762+GeorgeDiFox@users.noreply.github.com>
2023-02-07 16:50:18 +0300

    Added a currency model and provider.

---
+++ smtcb/Models/Currency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smtcb.Models
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string IsoCode { get; set; }
+    }
+}
